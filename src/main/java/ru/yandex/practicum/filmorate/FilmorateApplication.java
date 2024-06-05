e7245358b38f1cf4001933ad6d6a7a3c75c1169f
pan1dan <panonedan@yandex.ru>
2024-06-05 22:18:32 +0500

    2-nd correction

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,8 +1,5 @@
 package ru.yandex.practicum.filmorate;
 
-import ch.qos.logback.classic.Level;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
