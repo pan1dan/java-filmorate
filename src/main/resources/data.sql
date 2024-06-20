1d63a9b1aa96770b6da3a96561ffa1adf3a2d494
pan1dan <panonedan@yandex.ru>
2024-06-21 01:27:23 +0500

    Исправлен файл data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,31 @@
-INSERT INTO genres (genre_id, genre_name)
-    VALUES (1, 'Комедия'),
-           (2, 'Драма'),
-           (3, 'Мультфильм'),
-           (4, 'Триллер'),
-           (5, 'Документальный'),
-           (6, 'Боевик');
+MERGE INTO genres AS target
+USING (VALUES
+    (1, 'Комедия'),
+    (2, 'Драма'),
+    (3, 'Мультфильм'),
+    (4, 'Триллер'),
+    (5, 'Документальный'),
+    (6, 'Боевик')
+) AS source (genre_id, genre_name)
+ON target.genre_id = source.genre_id
+WHEN MATCHED THEN
+    UPDATE SET genre_name = source.genre_name
+WHEN NOT MATCHED THEN
+    INSERT (genre_id, genre_name)
+    VALUES (source.genre_id, source.genre_name);
 
-INSERT INTO film_rating_mpa (mpa_id, mpa_name)
-    VALUES (1, 'G'),
-           (2, 'PG'),
-           (3, 'PG-13'),
-           (4, 'R'),
-           (5, 'NC-17');
+MERGE INTO film_rating_mpa AS target
+USING (VALUES
+    (1, 'G'),
+    (2, 'PG'),
+    (3, 'PG-13'),
+    (4, 'R'),
+    (5, 'NC-17')
+) AS source (mpa_id, mpa_name)
+ON target.mpa_id = source.mpa_id
+WHEN MATCHED THEN
+    UPDATE SET mpa_name = source.mpa_name
+WHEN NOT MATCHED THEN
+    INSERT (mpa_id, mpa_name)
+    VALUES (source.mpa_id, source.mpa_name);
+
